07ad4e23a8a76a340f4e14c4313ba9064e3c173b
admin <teay10259@gmail.com>
2025-12-28 14:24:00 +0300

    tupo

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const https = require('https');
 const fs = require('fs');
+const path = require('path');
 
 const hostname = '0.0.0.0';
 const port = 3001;
 
 // Читаем сертификаты
 const options = {
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
+  key: fs.readFileSync(path.join(__dirname, 'server.key')),
+  cert: fs.readFileSync(path.join(__dirname, 'server.cert'))
 };
+
+// Читаем HTML файл
+const htmlFilePath = path.join(__dirname, 'index.html');
+const htmlContent = fs.readFileSync(htmlFilePath, 'utf-8');
 
 const server = https.createServer(options, (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-  res.end('<h1>Привет, мир через HTTPS!</h1>');
+  res.end(htmlContent);
 });
 
 server.listen(port, hostname, () => {
   console.log(`HTTPS сервер запущен на https://${hostname}:${port}/`);
 });
+
