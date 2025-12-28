9727dc01b7e0c3ba6deaa19c59eb326632eadb20
admin <teay10259@gmail.com>
2025-12-28 14:17:32 +0300

    gf

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-// файл: server.js
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
+  // Указываем UTF-8
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
   res.end('<h1>Привет, мир!</h1>');
 });
 
